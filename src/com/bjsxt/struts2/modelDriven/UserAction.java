58d14efa53379980e38c1aeb2d05d0af6701c08a
javanewcomer <tomsubscriber@gmail.com>
2014-09-16 20:48:30 +0200

    using model driven to pass parameter, implement ModelDriven interface

---
+++ src/com/bjsxt/struts2/modelDriven/UserAction.java
@@ -0,0 +1,21 @@
+package com.bjsxt.struts2.modelDriven;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+public class UserAction extends ActionSupport implements ModelDriven<User>{
+	
+	private User user = new User();
+	
+	public String add() {
+		System.out.println("name=" + user.getName());
+		System.out.println("age=" + user.getAge());
+		return SUCCESS;
+	}
+
+	@Override
+	public User getModel() {
+		return user;
+	}
+	
+}
